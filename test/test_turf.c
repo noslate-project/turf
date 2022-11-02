524192cdd94fb0bfb615fd03ad1b18e9c2fceb6b
Chengzhong Wu <chengzhong.wcz@alibaba-inc.com>
2022-11-02 16:54:17 +0800

    fix: abort if execve failed (#3)

--- test/test_turf.c
+++ test/test_turf.c
@@ -41,4 +41,45 @@ spec("turf") {
     rc = tf_action(&cfg);
     check(rc == 0);
   }
+
+  it("turf.start executable not found") {
+    int rc;
+    struct tf_cli cfg = {0};
+    char dest[1024];
+
+    shl_path3(dest, 1024, getenv("TURF_WORKDIR"), "/bundle", "no-executable");
+    puts(dest);
+
+    chdir(dest);
+    cfg.sandbox_name = "utest-no-executable";
+    // clean previous
+    cfg.cmd = TURF_CLI_REMOVE;
+    rc = tf_action(&cfg);
+
+    cfg.cmd = TURF_CLI_CREATE;
+    rc = tf_action(&cfg);
+    check(rc == 0);
+
+    cfg.cmd = TURF_CLI_LIST;
+    rc = tf_action(&cfg);
+    check(rc == 0);
+
+    cfg.cmd = TURF_CLI_START;
+    rc = tf_action(&cfg);
+    check(rc == -1);
+
+    // state:
+    cfg.cmd = TURF_CLI_STATE;
+    rc = tf_action(&cfg);
+    check(rc == -1);
+
+    // stop
+    cfg.cmd = TURF_CLI_STOP;
+    rc = tf_action(&cfg);
+    check(rc == -1);
+
+    cfg.cmd = TURF_CLI_REMOVE;
+    rc = tf_action(&cfg);
+    check(rc == 0);
+  }
 }
